6af92a114b67a2004216d9ef3c0a625aff6902f9
Davi Albino <negroalbino_@hotmail.com>
2022-07-19 15:38:31 -0300

    Adicionando requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utilities/renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testando Favorirte Pokémons', () => {
+  test('Testando o título da página', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading', {
+      name: /Page requested not found /i,
+      level: 2,
+    });
+
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  test('Verifica se About contém a imagem correta', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundPageImage = screen.getAllByRole('img');
+
+    expect(notFoundPageImage[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
