6ceb65463c3557390969fc2968bb2a337f81bdb8
Davi Albino <negroalbino_@hotmail.com>
2022-07-19 15:23:31 -0300

    Adicionando o requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utilities/renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+describe('Testando Favorirte Pokémons', () => {
+  test('Testando o título da página', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const favoriteTitle = screen.getByRole('heading', {
+      nome: /Favorite pokémons/i,
+      level: 2,
+    });
+
+    expect(favoriteTitle).toBeInTheDocument();
+  });
+
+  test('Testando se aparece mensagem correspondente sem favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavoriteFound = screen.getByText(/No favorite pokemon found/i);
+
+    expect(noFavoriteFound).toBeInTheDocument();
+  });
+});
